898c98ed35130c75398adbeb11dc89a767b507d5
fralch <ingfralch@gmail.com>
2016-09-27 19:57:38 -0500

    json de ruta creado y funcionando falta medir distancia

--- haku/www/js/main.js
+++ haku/www/js/main.js
@@ -75,7 +75,7 @@ function medir(latx,lony) {
 
 		for (var i in capillas) {
  			var med_aux = google.maps.geometry.spherical.computeDistanceBetween( origen, capillas[i] );
- 			console.log(med_aux);
+ 			//console.log(med_aux);
 
  			if (med_aux < med) {
  				med = med_aux;
@@ -83,6 +83,21 @@ function medir(latx,lony) {
  				
  			};
 		};
-		console.log('este es :'+med+' metros');
+		//console.log('este es :'+med+' metros');
+		console.log(rutas.ucci.waypts[0].location)
+		
 }
 
+var rutas = {
+				ucci: {
+					inicio: {lat: -12.047208967661641, lng: -75.19922867417336},
+					final: {lat: -12.06861605050217, lng: -75.21021097898483},
+					waypts: [
+								{location: new google.maps.LatLng(-12.051727018076974,-75.20204901695251)},
+								{location: new google.maps.LatLng(-12.05703610331996,-75.20507454872131)},
+								{location: new google.maps.LatLng(-12.060895092338846,-75.20760118961334)},
+								{location: new google.maps.LatLng(-12.065919668405291,-75.20520597696304)},
+								{location: new google.maps.LatLng(-12.067400058721029,-75.20796597003937)}
+							]
+					  } 
+}
